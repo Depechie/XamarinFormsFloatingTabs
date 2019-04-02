d49e98831ef5e7c62fb84a3ad0903f1991fb2e69
depechie <glenn.versweyveld@gmail.com>
2019-04-02 08:36:22 +0200

    Enable icon fonts in floating tabs

--- XamarinFloatingTabs/Views/OverlayTabView.xaml.cs
+++ XamarinFloatingTabs/Views/OverlayTabView.xaml.cs
@@ -7,24 +7,38 @@ namespace XamarinFloatingTabs.Views
         public OverlayTabView()
         {
             InitializeComponent();
+            Tab1.TextColor = Color.Gray;
+            Tab2.TextColor = Tab3.TextColor = Color.DarkGray;
         }
 
         private void OnDiscoveryClicked(object sender, System.EventArgs e)
         {
             if (App.Current.MainPage is TabbedPage mainPage && sender is Button button)
-                mainPage.CurrentPage = mainPage.Children[0];            
+            {
+                mainPage.CurrentPage = mainPage.Children[0];
+                Tab1.TextColor = Color.Gray;
+                Tab2.TextColor = Tab3.TextColor = Color.DarkGray;
+            }
         }
 
         private void OnSearchClicked(object sender, System.EventArgs e)
         {
             if (App.Current.MainPage is TabbedPage mainPage && sender is Button button)
+            {
                 mainPage.CurrentPage = mainPage.Children[1];
+                Tab2.TextColor = Color.Gray;
+                Tab1.TextColor = Tab3.TextColor = Color.DarkGray;
+            }
         }
 
         private void OnBookmarksClicked(object sender, System.EventArgs e)
         {
             if (App.Current.MainPage is TabbedPage mainPage && sender is Button button)
+            {
                 mainPage.CurrentPage = mainPage.Children[2];
+                Tab3.TextColor = Color.Gray;
+                Tab1.TextColor = Tab2.TextColor = Color.DarkGray;
+            }
         }
     }
 }
